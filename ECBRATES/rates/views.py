2eb9295edc8d8f40accc54c190a6572c7e484e60
Wincij <m.pajaczkowski95@gmail.com>
2019-03-13 01:29:11 +0100

    getLinks() updated

--- ECBRATES/rates/views.py
+++ ECBRATES/rates/views.py
@@ -12,16 +12,13 @@ import requests
 """
 
 def getLinks():
+    """ This function returns complete list of necessary RSS channels from official ECB website"""
     page = 'https://www.ecb.europa.eu/home/html/rss.en.html'
     request = requests.get(page)
     html = request.content.decode('utf-8')
     soup = BeautifulSoup(html, 'html.parser')
-
     currencies = soup.find_all('a', class_="rss")
-
     link_list = []
-
-
     for currency in currencies:
         # Some RSS's are for other purposes than getting currencies rates. All used for currencies contains fxref in href
         if 'fxref' in currency['href']:
