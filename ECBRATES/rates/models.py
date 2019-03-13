d3ac73348fc5ae8c6a77354482f8c5d8dd3aea17
Wincij <m.pajaczkowski95@gmail.com>
2019-03-13 01:43:18 +0100

    Rate model created

--- ECBRATES/rates/models.py
+++ ECBRATES/rates/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Rate(models.Model):
+    currency = models.CharField(max_length = 40, null = False)
+    cost = models.FloatField(default = None)
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
