dd6852bf0fa33066da254aec69e4c135ad72185e
Wincij <m.pajaczkowski95@gmail.com>
2019-03-13 01:26:46 +0100

    getLinks() function created.

--- ECBRATES/rates/views.py
+++ ECBRATES/rates/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from bs4 import BeautifulSoup
+import requests
+
+
+
+"""
+    First we use reuquests to get every currency RSS from https://www.ecb.europa.eu/home/html/rss.en.html.
+    We use BeautifulSoup to get all content. We will use this link list later
+"""
+
+def getLinks():
+    page = 'https://www.ecb.europa.eu/home/html/rss.en.html'
+    request = requests.get(page)
+    html = request.content.decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
+
+    currencies = soup.find_all('a', class_="rss")
+
+    link_list = []
+
+
+    for currency in currencies:
+        # Some RSS's are for other purposes than getting currencies rates. All used for currencies contains fxref in href
+        if 'fxref' in currency['href']:
+            link_list.append("https://www.ecb.europa.eu" + currency['href'])
+
+    # We crated completed list of RSS
+    return link_list
